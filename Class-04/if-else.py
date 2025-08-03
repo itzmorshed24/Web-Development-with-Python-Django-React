9f692776c0200eca53ddf8a60b78bbc0123b5bf5
itzmorshed24 <itzmorshed24@gmail.com>
2025-08-03 08:31:06 +0600

    Learn Control Flow Statement

---
+++ Class-04/if-else.py
@@ -0,0 +1,7 @@
+#if-else statement
+
+age = 17
+if age>=18:
+    print("You are an adult")
+else:
+    print("You are a child.")
---
+++ Class-04/neasted-if-else.py
@@ -0,0 +1,16 @@
+mark = 35
+
+if mark<=100 and mark>=80:
+    print("A+")
+elif mark<80 and mark>=70:
+    print("A")
+elif mark<70 and mark>=60:
+    print("A-")
+elif mark<60 and mark>=50:
+    print("B")
+elif mark<50 and mark>=40:
+    print("C")
+elif mark<40 and mark>=33:
+    print("D")
+else:
+    print("F")
